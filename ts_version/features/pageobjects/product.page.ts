47284ef781f5c93e53719e79e2e584c803f885f5
kkaleinikava-lvrx <katsiaryna.kaleinikava@leverx.com>
2025-11-17 19:49:03 +0200

    Added Typescript config and script
    Added Typescript config and reworked ProductCatalog feature for TS

---
+++ ts_version/features/pageobjects/product.page.ts
@@ -0,0 +1,93 @@
+class ProductPage {
+     
+    static ADD_TO_CART_BUTTON_SELECTOR = {
+        "elementProperties": {
+            "viewName": "sap.ui.demo.cart.view.Product",
+            "metadata": "sap.m.Button",
+            "text": [
+                {
+                    "path": "i18n>addToCartShort"
+                }
+            ]
+        }
+    }
+    
+    static PRODUCT_IMAGE_SELECTOR = {
+        "elementProperties": {
+            "viewName": "sap.ui.demo.cart.view.Product",
+            "metadata": "sap.m.Image",
+            "id": "*productImage"
+        }
+    }
+
+    static PRODUCT_NAME_SELECTOR = {
+        "elementProperties": {
+            "viewName": "sap.ui.demo.cart.view.Product",
+            "metadata": "sap.m.Text",
+            "id": "*titleText"
+        }
+    }
+
+    static PRODUCT_PRICE_SELECTOR = {
+        "elementProperties": {
+            "viewName": "sap.ui.demo.cart.view.Product",
+            "metadata": "sap.m.ObjectNumber"
+        }
+    }
+
+    static PRODUCT_STATUS_SELECTOR = {
+        "elementProperties": {
+            "viewName": "sap.ui.demo.cart.view.Product",
+            "metadata": "sap.m.ObjectStatus"
+        }
+    }
+
+    static SHOW_SHOPPING_CART_BUTTON_SELECTOR = {
+        "elementProperties": {
+            "viewName": "sap.ui.demo.cart.view.Product",
+            "metadata": "sap.m.ToggleButton",
+            "tooltip": [
+                {
+                    "model": "i18n",
+                    "path": "toCartButtonTooltip",
+                    "value": "Show Shopping Cart",
+                    "type": "string"
+                }
+            ]
+        }
+    }
+
+    async addProductToCart(): Promise<void> {
+        await this.clickAddToCartButton();
+        if (await this.getProductStatus() === "Out of Stock") {
+            await ui5.confirmationDialog.clickOk();
+        }
+    }
+
+    async clickAddToCartButton(): Promise<void> {
+        await ui5.userInteraction.click(ProductPage.ADD_TO_CART_BUTTON_SELECTOR);
+    }
+
+    async getProductName(): Promise<string> {
+        return await ui5.element.getPropertyValue(ProductPage.PRODUCT_NAME_SELECTOR, "text");
+    }
+
+    async getProductPrice(): Promise<string> {
+        return await ui5.element.getPropertyValue(ProductPage.PRODUCT_PRICE_SELECTOR, "number");
+    }
+
+    async getProductStatus(): Promise<string> {
+        return await ui5.element.getPropertyValue(ProductPage.PRODUCT_STATUS_SELECTOR, "text");
+    }
+    
+    async showShoppingCart(): Promise<void> {
+        await ui5.userInteraction.click(ProductPage.SHOW_SHOPPING_CART_BUTTON_SELECTOR);
+    }
+
+    async waitForPageLoaded(): Promise<void> {
+        await ui5.element.getDisplayed(ProductPage.PRODUCT_IMAGE_SELECTOR);        
+    }
+
+}
+     
+export default new ProductPage();
---
+++ ts_version/features/pageobjects/shoppingCart.page.ts
@@ -0,0 +1,66 @@
+class ShoppingCartPage {
+     
+    static CART_ENRTY_SELECTOR = {
+        "elementProperties": {
+            "viewName": "sap.ui.demo.cart.view.Cart",
+            "metadata": "sap.m.ObjectListItem",
+            "bindingContextPath": "/cartEntries/*"
+        }
+    }
+    
+    static PAGE_TITLE_SELECTOR = {
+        "elementProperties": {
+            "viewName": "sap.ui.demo.cart.view.Cart",
+            "metadata": "sap.m.Title",
+            "id": "*page-title"
+        }
+    }
+
+    static PROCEED_BUTTON_SELECTOR = {
+        "elementProperties": {
+            "viewName": "sap.ui.demo.cart.view.Cart",
+            "metadata": "sap.m.Button",
+            "id": "*proceedButton"
+        }
+    }
+
+    async clickProceedButton(): Promise<void> {
+        await ui5.userInteraction.click(ShoppingCartPage.PROCEED_BUTTON_SELECTOR);  
+    }
+
+    async getItemListInShoppongCart(): Promise<Array<object>> {
+        const itemsInCartArray = [];
+        for (let i = 0; i < await this.getQuantityOfItemsInShoppingCart(); i++) {
+            itemsInCartArray.push({ 
+                name: await ui5.element.getPropertyValue(ShoppingCartPage.CART_ENRTY_SELECTOR, "title", i), 
+                price: await ui5.element.getPropertyValue(ShoppingCartPage.CART_ENRTY_SELECTOR, "number", i),
+                quantity: parseInt(await ui5.element.getPropertyValue(ShoppingCartPage.CART_ENRTY_SELECTOR, "intro", i))
+            });
+        }
+        return itemsInCartArray;
+    }
+
+    async getQuantityForProductFromShoppingCart(productName: string): Promise<number> {
+        const productSelector = {
+            "elementProperties": {
+                "viewName": "sap.ui.demo.cart.view.Cart",
+                "metadata": "sap.m.ObjectListItem",
+                "title": productName,
+                "bindingContextPath": "/cartEntries/*"
+            }
+        }
+        return parseInt(await ui5.control.getProperty(productSelector, "intro"));
+    }
+
+    async getQuantityOfItemsInShoppingCart(): Promise<number> {      
+        const cartItemsArray = await ui5.element.getAllDisplayed(ShoppingCartPage.CART_ENRTY_SELECTOR);
+        return cartItemsArray.length;
+    }
+    
+    async waitForPageLoaded(): Promise<void> {
+        await ui5.element.waitForAll(ShoppingCartPage.PAGE_TITLE_SELECTOR);        
+    }
+    
+}
+     
+export default new ShoppingCartPage();
